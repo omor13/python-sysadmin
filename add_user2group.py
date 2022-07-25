349054c355a3f7cdc03088f9781172905c0b8cc3
omor13 <fnsfbk@gmail.com>
2022-07-25 11:24:01 +0300

    commit - add user to group

--- add_user2group.py
+++ add_user2group.py
@@ -5,7 +5,7 @@ def add_user_to_group():
     output = subprocess.Popen('groups', stdout=subprocess.PIPE).communicate()[0]
     print("Enter a list of groups to add the user to")
     print("The list should be separated by spaces, for example:\r\n group1 group2 group3")
-    print("The available groups are:\r\n " + output)
+    print("The available groups are:\r\n " + str(output))
     chosenGroups = str(input("Groups: "))
 
 add_user_to_group()
